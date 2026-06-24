7d1529590c86849c473355000df4041512d9abc0
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2026-06-24 19:54:46 +0300

    Add files via upload

---
+++ raq93.com.js
@@ -0,0 +1,356 @@
+var _0x2409 = ['.nav-tabs', 'insertBefore', `<div id="des333">
+\
+
+\
+</div>
+\
+
+\
+
+\
+`, `<form name="jump">
+\
+<div class="MyColors">
+\
+					<map id="ColorsMap" name="ColorsMap">
+\
+					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
+\
+					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
+\
+					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
+\
+					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
+\
+					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
+\
+					<area alt="" coords="122, 0, 147, 25" href="javascript:chooseStyle('Co6',60)" shape="rect" title="6">
+\
+					<area alt="" coords="92, 0, 117, 25" href="javascript:chooseStyle('Co7',60)" shape="rect" title="7">
+\
+					</map>
+\
+			     
+</form>
+\
+
+\
+`, 'https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js', 'getScript', '.loginstat', 'insertAfter', "<div class='fr borderg minix' style='padding:2px;background-color:white;margin-top: 4px;margin-right: 5px;'>  استايلات و تصاميم :<a href=''>رمــادي</a></div>", "</div>", '1px 0', '2px solid #434343', '1px solid #4F565E', '1px solid #434343', '2px', '3px', 'css', '.hand.nosel.fl.uzr.border', '<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">', 'html', '.checkbox label', 'body', `<link rel="stylesheet" type="text/css" href="">
+\
+<link href="https://raw.githack.com/noorromas/iraq93.com/main/raq93.com.css" rel="stylesheet" type="text/css" />
+
+\
+<style>
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+}
+\
+
+\
+img.co {
+    transform: rotate(100deg);
+    margin-top: 5px!important;
+\
+}
+\
+label.label.label-primary.mini.fl {
+\
+    margin-left: -32px!important;
+\
+}
+\
+
+span#s1 {
+    margin-top: 0px;
+    position: absolute;
+    margin-left: 179px;
+\
+
+\
+}
+\
+span.flex-grow-1.dots {
+\
+    width: 60%!important;
+\
+}
+\
+ img.fl, span.btn.fr.btn-success.fa.fa-refresh {
+\
+    display: none
+\
+}
+\
+
+\
+a.label.label-primary.fr {
+\
+    transform: matrix(1, 0, 0, 1, -94, 0);
+\
+}
+\
+
+\
+.fr.borderg, .fr.borderg a {
+\
+    font-size: 10px!important;
+\
+    padding: 0 5px!important;
+\
+    position: relative;
+\
+    color: #fff;
+\
+	font-family: jazeera,FontAwesome;
+\
+}
+\
+
+\
+.fr.borderg {
+\
+    border: 0px;
+\
+    height: 0;
+\
+}
+\
+ul.nav.nav-tabs {
+    border-bottom: 9px #5e635c solid!important;
+    float: right;
+    padding: 10px 6px 2px 1px;
+    border-radius: 0;
+    margin-top: 0;
+    border-left: 2.9px solid #5e635c;
+    border-right: 2.9px solid #5e635c;
+    background-image: -webkit-linear-gradient(160deg,#f9f9f9 25%,#728ea1 25%,#d4d4d4 50%,#728ea1 50%,#d4d4d4 75%,#d4d4d4 75%,#f9f9f9)!important;
+    background: #ebebeb;
+    border-top: 1px #5e635c solid;
+    border-radius: 5%;
+    border-style: dashed;
+    height: initial!important;
+\
+
+\
+}
+\
+div#tlogins ::-webkit-scrollbar {
+\
+    width: 0px;
+\
+    height: 10px
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-button {
+\
+    width: 5px;
+\
+    height: 5px
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-corner {
+\
+    background: 0 0
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-thumb {
+\
+    background: #434343;
+\
+    border: 71px #fff;
+\
+    border-radius: 31px;
+\
+    background-image: -webkit-linear-gradient( 45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-track {
+\
+    background: #fff;
+\
+    border: 65px #fff;
+\
+    border-radius: 34px
+\
+}
+\
+.uhtml {
+    text-align: left;
+    width: 100%;
+    margin: 0px!important;
+    border-bottom: 1px solid lavender;
+    padding-right: 1px;
+    background-color: #fafafa;
+
+}
+.uzr .fitimg {
+    background-size: cover;
+    margin: 0;
+    border-radius: 5px;
+    border: 2px solid #595959;
+    width: 50px!important;
+    box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%), 0 1px 8px #cbb0c8;
+    height: 50px!important;
+}
+\
+#des0 {
+\
+    border: 0px dotted#f0ad4e;
+\
+    text-align: center;
+\
+    border-bottom: 0 solid#fff!important;
+\
+    border-right: .5px solid#fff!important;
+\
+    border-top: .5px solid#fff!important;
+\
+    border-left: .5px solid#fff!important;
+\
+    height: 20px;
+\
+    color: #fff;
+\
+    background-color: #434343!important;
+\
+    margin-bottom: -3px;
+\
+    font-family: jazeera,FontAwesome;
+\
+    font-size: 13px!important;
+\
+    margin-right: -1px;
+\
+    line-height: 10px;
+\
+    margin-top: 12px;
+\
+	border-radius: 0 0 30px 30px;
+\
+    width: 100%;
+\
+	box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 -3px 1px -2px rgba(0,0,0,.2)!important;
+\
+}
+\
+div#tlogins .fl.hand.nosel.uzr {
+    border-radius: 5px;
+    margin-bottom: 2px!important;
+    box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%), 0 1px 8px #000;
+    margin-top: 2px!important;
+    margin: 1px 0px;
+}
+\
+
+\
+.des2 {
+\
+    background-color: #fff;
+\
+    border-radius: 0;
+\
+    margin: 3px 8px!important;
+\
+    border-style: solid;
+\
+    border-right: 7px solid;
+\
+    border-left: 7px solid;
+\
+    border-color: #fcf8e3;
+\
+    border-left-style: double;
+\
+    border-right-style: double;
+\
+    font-family: jazeera,FontAwesome;
+\
+    font-size: 13px!important
+}
+
+#d2bc .uzr.d-flex.mm:nth-child(odd) {
+    background-color: rgb(255 255 255 / 20%) !important;
+}
+
+#chats{background-color:#d4cfd2!important;background-image:url(https://up6.cc/2026/06/178231970002621.jpg);background-size:100%}
+}
+#wall .uzr.d-flex.mm{border:1px solid transparent!important}#d2bc{background-image:url(https://up6.cc/2024/11/17324748993091.png);background-size:100%}
+}
+#wall .fitimg.u-pic.borderg{height:52px!important;border:1px solid;border-radius:100px;box-shadow:0 0 0 #000000,inset 0 0 4px #000000,0 0 0 0 #000;margin-top:2px;margin-right:2px}
+\
+}
+\
+
+\
+label.fl.label.loginstat {
+    position: absolute;
+    padding: 2px 8px 2px 8px!important;
+    font-size: 15px!important;
+    margin-left: 1px!important;
+    margin-top: 1px!important;
+    height: 24px!important;
+    border-radius: 70px 0px 0px 0px!important;
+}
+#d2bc .uzr.d-flex.mm:nth-child(even) {
+    background-color: #f7f7f700 !important;
+
+
+\
+}
+\
+label.addGruMsg.label.tc.border.btn.label-info.fl {
+\
+    margin-top: 0px!important;
+\
+}
+\
+
+\
+</style>`];
+$(`${''}${_0x2409[2]}${''}`)[_0x2409[1]](_0x2409[0]), $(`${''}${_0x2409[3]}${''}`)[_0x2409[1]](_0x2409[0]), $[_0x2409[5]](_0x2409[4]), $(_0x2409[8])[_0x2409[7]]($(_0x2409[6])), $(_0x2409[9])[_0x2409[7]]($(_0x2409[6])), $(_0x2409[17])[_0x2409[16]]({
+    margin: _0x2409[10],
+    borderRight: _0x2409[11],
+    borderLeft: _0x2409[11],
+    borderTop: _0x2409[12],
+    borderBottom: _0x2409[13],
+    paddingTop: _0x2409[14],
+    borderRadius: _0x2409[15]
+}), $(_0x2409[20])[_0x2409[19]](`${''}${_0x2409[18]}${''}`), $(`${''}${_0x2409[22]}${''}`)[_0x2409[1]](_0x2409[21])
